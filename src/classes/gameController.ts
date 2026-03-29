6a5b34f636ac44211e34e33b5a3ab0b56ba819f7
Dan-DH <daniel.diaz.hdez@gmail.com>
2026-03-29 13:39:55 +0200

    fix: corrected turn counter

--- src/classes/gameController.ts
+++ src/classes/gameController.ts
@@ -119,7 +119,7 @@ export class GameController {
     // Add a generic gameobject pointer event to make it easier to hide a unit info card
     context.input.on('gameobjectdown', () => visibleUnitCardCheck(context));
 
-    context.add.text(850, 110, `TURN ${context.currentGame.turnNumber + 1}`, {
+    context.add.text(850, 110, `TURN ${context.currentGame.turnNumber }`, {
       fontFamily: 'proHeavy',
       fontSize: 35,
       color: '#ffffff'
